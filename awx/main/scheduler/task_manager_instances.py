c126cd381153a1ecf1c60270d800fb8b2a4ac0bb
Elijah DeLee <kdelee@redhat.com>
2022-01-29 14:44:57 -0500

    tweak class names and fix some typos

--- awx/main/scheduler/task_manager_instances.py
+++ awx/main/scheduler/task_manager_instances.py
@@ -21,7 +21,7 @@ from awx.main.models import (
 logger = logging.getLogger('awx.main.scheduler')
 
 
-class PrioritizableControlNode:
+class PrioritizableNode:
     def __init__(self, remaining_capacity, hostname):
         self.remaining_capacity = remaining_capacity
         self.hostname = hostname
@@ -45,27 +45,32 @@ class PrioritizableControlNode:
         return self.__str__()
 
 
-class PrioritizedControlNodes:
+class PrioritizedNodes:
     def __init__(self):
-        self.control_nodes = set()
+        self.nodes = set()
 
     def add(self, node):
-        self.control_nodes.add(node)
+        self.nodes.add(node)
 
     def update(self, node):
-        """The __hash__ function for PrioritizableControlNodes just compares hostnames.
+        """The __hash__ function for PrioritizableNodes just compares hostnames.
 
-        We can add/remove the node to update object in PrioritizedControlNodes to have right capacity.
+        We can add/remove the node to update object in PrioritizedNodes to have right capacity.
         """
-        self.control_nodes.remove(node)
-        self.control_nodes.add(node)
+        self.nodes.remove(node)
+        self.nodes.add(node)
+
+    def best_node(self):
+        heap = list(self.nodes)
+        heapq.heapify(heap)
+        return heapq.nlargest(1, heap).pop()
 
 
 class TaskManagerInstances:
     def __init__(self):
         realinstances = Instance.objects.filter(hostname__isnull=False, enabled=True).exclude(node_type='hop').prefetch_related('rampart_groups')
         self.instances_partial = dict()
-        self.control_nodes = PrioritizedControlNodes()
+        self.control_nodes = PrioritizedNodes()
 
         for instance in realinstances:
             self.instances_partial[instance.hostname] = SimpleNamespace(
@@ -78,7 +83,7 @@ class TaskManagerInstances:
                 instance_groups=[ig.name for ig in instance.rampart_groups.all()],
             )
             if instance.node_type in ('control', 'hybrid'):
-                self.control_nodes.add(PrioritizableControlNode(remaining_capacity=instance.remaining_capacity, hostname=instance.hostname))
+                self.control_nodes.add(PrioritizableNode(remaining_capacity=instance.remaining_capacity, hostname=instance.hostname))
 
     def __getitem__(self, key):
         return self.instances_partial.get(key)
@@ -102,9 +107,7 @@ class TaskManagerInstances:
 
         If no node is available, return False
         """
-        control_heap = list(self.control_nodes)
-        heapq.heapify(control_heap)
-        best_instance = heapq.nlargest(1, control_heap).pop()
+        best_instance = self.control_nodes.best_node()
         if best_instance.remaining_capacity < task.task_impact:
             return False
 
@@ -115,9 +118,9 @@ class TaskManagerInstances:
             for ig in self.instances_partial[best_instance.hostname].instance_groups:
                 ig_data = ig_capacity_graph[ig]
                 if not ig_data['control_capacity'] - ig_data['consumed_control_capacity'] >= task.task_impact:
-                    logger.warn(f"Somehow we had capacity on a control node {best_instance} but not on its instance_group {instance_group}")
+                    logger.warn(f"Somehow we had capacity on a control node {best_instance} but not on its instance_group {ig}")
                     return False
-        logger.debug(f"chose {best_instance} to run {task.log_format}")
+        logger.debug(f"chose {best_instance} to be control node for {task.log_format}")
         best_instance.remaining_capacity -= task.task_impact
-        self.update(best_instance)
+        self.control_nodes.update(best_instance)
         return best_instance
